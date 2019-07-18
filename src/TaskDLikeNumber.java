67da8b5f0a1a3d64237ece775a2e8d1ca5f8e61e
Sergey Petryashev <petsero@mail.ru>
2019-07-18 18:07:56 +0300

    edit solve TaskD

--- src/TaskDLikeNumber.java
+++ src/TaskDLikeNumber.java
@@ -2,8 +2,10 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 /* There is a number N, not containing zeros. Do K times the following: 
  * select two numbers that are in different positions, and swap them.
  * Find the probability that the final number will be divided by at least 
@@ -17,46 +19,64 @@ public class TaskDLikeNumber {
         int number = Integer.parseInt(br.readLine());
         int numShift = Integer.parseInt(br.readLine());
         int [] numbers = didgitNumb(number);
-        List<Integer> listNumb =new ArrayList<>();
-        List<Integer> listNumbTemp =new ArrayList<>();
-        List<Integer> listNumbAll =new ArrayList<>();
+        Map<Integer, Long>  listNumb =new HashMap<>();
+        Map<Integer, Long> listNumbTemp =new HashMap<>();
+        Map<Integer, Long> listNumbCurrent;
         for(int shift=0; shift<numShift;shift++) {
+        	listNumbCurrent=new HashMap<>();
 			if(shift!=0) {
-				for(int i=0;i<listNumb.size();i++) {
-					int [] num = didgitNumb(listNumb.get(i));
+				Set<Map.Entry<Integer, Long>> setNumb = listNumb.entrySet();
+				for(Map.Entry<Integer, Long> me : setNumb) {
+					int [] num = didgitNumb(me.getKey());
 					listNumbTemp=shiftNumber (num, num.length);
-					for(int e :listNumbTemp) {
-						if(!listNumbAll.contains(e))
-							listNumbAll.add(e);
+					Set<Map.Entry<Integer, Long>> set = listNumbTemp.entrySet();
+					for(Map.Entry<Integer, Long> e : set) {
+						if(!listNumbCurrent.containsKey(e.getKey())) {
+							listNumbCurrent.put(e.getKey(),e.getValue()*me.getValue());
+						}else {
+							listNumbCurrent.put(e.getKey(),listNumbCurrent.get(e.getKey())+e.getValue()*me.getValue());	// текущее значение + новое значение
+						}
 					}
 				}
-				listNumb=listNumbAll;
+				//=========================================================
+				// проверка на общий делитель
+				Set<Map.Entry<Integer, Long>> setNumbCurrent = listNumbCurrent.entrySet();
+				long divide = maxDel(setNumbCurrent);
+				if(divide>1) {
+					for(Map.Entry<Integer, Long> eNC : setNumbCurrent) {
+						listNumbCurrent.put(eNC.getKey(),listNumbCurrent.get(eNC.getKey())/divide);
+					}
+				}//*/
+				//======================================================
+				listNumb=new HashMap(listNumbCurrent);	// копирование чисел на следующие шаг перестановки
 			}else if(shift==0) {
 				listNumb=shiftNumber (numbers, numbers.length);
-			}                  	
+			}			
         }
-//        System.out.println(listNumbAll);
-        System.out.println(listNumb);
-        if(numShift!=1 && numShift!=0) {
-        	probably(listNumbAll);
-        }else if(numShift==0){
-        	listNumb.add(number);
+//        System.out.println(listNumb);
+        if(numShift==0){
+        	listNumb.put(number,(long)1);
         	probably(listNumb);
         }else {
         	probably(listNumb);
         }
+        br.close();
 	}
 	
-	private static List<Integer> shiftNumber(int [] number, int didg) {
-		List<Integer> list =new ArrayList<>();
+	private static Map<Integer,Long> shiftNumber(int [] number, int didg) {
+		Map<Integer,Long> list =new HashMap<>();
 		int [] newNumber = new  int [didg];
 		int shiftNumber;
 		for(int i=0; i<didg; i++) {
 			for(int j=i+1; j<didg; j++) {
 				newNumber = swap(number, i , j);
 				shiftNumber = formNumber(newNumber);
-				if(!list.contains(shiftNumber))
-					list.add(shiftNumber);
+				if(!list.containsKey(shiftNumber)) {
+					list.put(shiftNumber,(long) 1);
+				}else {
+					
+					list.put(shiftNumber,list.get(shiftNumber)+1);
+				}
 //				System.out.print(shiftNumber+" ");
 			}
 		}
@@ -83,25 +103,58 @@ public class TaskDLikeNumber {
 		return arr;
 	}
 	
-	private static int [] didgitNumb(int numb) {
+	private static int [] didgitNumb(int number2) {
 		String s="";
-		int didgit = (s+numb).length();
+		int didgit = (s+number2).length();
 		int [] number = new int [didgit];
 		for(int i=0;i<didgit;i++) {
-			number[didgit-1-i]=numb%10;
-			numb=numb/10;
+			number[didgit-1-i]=number2%10;
+			number2=number2/10;
 		}
 		return number;
 	}
 	
-	private static void probably(List<Integer> list) {
-		int count=0;
-		for(int e : list) {
-			if(e%5==0 || e%6==0)
-				count++;
+	private static void probably(Map<Integer,Long> map) {
+		double trueCount=0;
+		double allCount=0;
+		Set<Map.Entry<Integer, Long>> set = map.entrySet();
+		for(Map.Entry<Integer, Long> me : set) {
+			allCount+=me.getValue();
+			if(me.getKey()%5==0 || me.getKey()%6==0)
+				trueCount+=me.getValue();
 		}
-		double pr = (double)count/(double)list.size();
+		double pr = trueCount/allCount;
 		System.out.printf("%.15f", pr);
+	}
+	
+	private static long maxDel (Set<Entry<Integer, Long>> setNum) {
+		long divide=1; 
+		long min=100000000;
+		boolean isDel=false;
+		for(Map.Entry<Integer, Long> e : setNum) {
+			if(e.getValue()<min) {
+				min=e.getValue();
+			}
+		}
+		divide=min+1;
+		boolean isOne=true;
+		while(!isDel && divide!=1) {
+			isDel=true;
+			if(!isOne && divide%2==0) {
+				divide/=2;
+			}else {
+				divide--;
+				isOne=false;
+			}
+			for(Map.Entry<Integer, Long> e : setNum) {
+				if(e.getValue()%divide!=0) {
+					isDel=false;
+				}
+			}			
+
+		}
+//		System.out.println("Min "+min+" Del "+divide);
+		return divide;
 	}
 
 }
